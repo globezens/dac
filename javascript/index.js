0d7dc199ee5b82fa373c886332df661880544fef
San  ヘ(◕。◕ヘ) <studiouspancake@hotmail.com>
2021-10-11 21:21:09 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,24 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 7.1;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+   window.addEventListener('scroll',
+            moveScrollIndicator);
+  
+    // Getting scrollIndicator element by ID
+    const scrollIndicatorElt = 
+        document.getElementById('scrollIndicator');
+  
+    // Height of entire web page - height
+    // of viewable portion of browser
+    const maxHeight = 
+        window.document.body.scrollHeight 
+        - window.innerHeight;
+  
+    function moveScrollIndicator(e) {
+  
+        // Calculating width of the 
+        // scrollIndicator element
+        const percentage = 
+            ((window.scrollY) / maxHeight) * 100;
+  
+        // Pixels scrolled by the user 
+        // to total scrollable Pixels
+        scrollIndicatorElt.style.width
+                 = percentage + '%';
