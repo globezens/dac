eb7f98dfefc691440e36fbe1b21ec8423afaa483
San  ヘ(◕。◕ヘ) <studiouspancake@hotmail.com>
2021-10-11 14:27:07 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,6 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 8.8;
+  var scrolled = (winScroll / height) * 8.6;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
