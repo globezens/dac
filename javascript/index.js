2cf3672ca0b50ab2a20e456a7cfd7060811e75c2
San  ヘ(◕。◕ヘ) <explorist20@gmail.com>
2021-10-11 12:04:51 +0530

    bar fix 2

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,6 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 10;
+  var scrolled = (winScroll / height) * 9.1;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
