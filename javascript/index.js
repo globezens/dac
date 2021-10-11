a36c9c12d5faaea84eb6b9b53ba0c8f4fa6b6baf
San  ヘ(◕。◕ヘ) <explorist20@gmail.com>
2021-10-11 11:53:58 +0530

    54565455

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,6 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 50;
+  var scrolled = (winScroll / height) * 0.1;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
